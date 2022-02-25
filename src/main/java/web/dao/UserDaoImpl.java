4fb161442a48e40d73be5206e753b94cc9d0a019
pahan <https://github.com/aristovp/PP_1>
2022-02-25 21:34:26 +0300

    upd 3.0

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,7 +1,5 @@
 package web.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 import javax.persistence.EntityManager;
